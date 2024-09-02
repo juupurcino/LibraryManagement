24f0a167770248e1739b130662d7f017c951e171
xmarimarquesh <marimarquesh39@gmail.com>
2024-09-02 16:50:56 -0300

    CSS corrigidos

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
  
 const database = new sequelize('PlotPit', 'PlotPit', 'etsps2024401',
 {
-    dialect: 'mssql', host:'localhost', port: 1434
+    dialect: 'mssql', host:'localhost', port: 1433
 });
  
 database.sync()
